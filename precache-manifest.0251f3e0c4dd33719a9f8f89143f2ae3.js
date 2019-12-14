efc05fdc7e791491aa611437e8f4a436314beb03
AlexHodovas <o.hodovas@gmail.com>
2019-12-14 17:00:39 +0200

    Updates

---
+++ precache-manifest.0251f3e0c4dd33719a9f8f89143f2ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a99a396756d3764ea04af4625f553b",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "6fb85f6f87738de3806f",
+    "url": "/react_todo-app/static/css/main.095e198b.chunk.css"
+  },
+  {
+    "revision": "e162f3baf9892877d47e",
+    "url": "/react_todo-app/static/js/2.4c2358ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo-app/static/js/2.4c2358ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fb85f6f87738de3806f",
+    "url": "/react_todo-app/static/js/main.c0e0f8cf.chunk.js"
+  },
+  {
+    "revision": "8034973f455e6d07c39f",
+    "url": "/react_todo-app/static/js/runtime-main.392eaa73.js"
+  }
+]);
